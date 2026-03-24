fa284cb597ba0c8827119ad3b6ef99ebf8e9b7ed
stu_macca <stuart@mcmillantendering.com.au>
2026-03-24 19:30:40 +1100

    fixed superbase url key

--- auth/auth-config.js
+++ auth/auth-config.js
@@ -1,5 +1,5 @@
 window.AUTH_CONFIG = window.AUTH_CONFIG || {
-  supabaseUrl: "https://wsksgnkqvjgsyvxfvxzh.supabase.co",
+  supabaseUrl: "https://wsksgnkqvjqsyvxfvxzh.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indza3Nnbmtxdmpxc3l2eGZ4dnpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5MTA4NjUsImV4cCI6MjA4OTQ4Njg2NX0.OPuTK47FmPTDPHQsJMhfU10I1Er7jiQtyVm7osRzDlA",
   resetPasswordUrl: "https://mcmillantendering.com.au/auth/reset-password/",
   setPasswordUrl: "https://mcmillantendering.com.au/auth/set-password/"
